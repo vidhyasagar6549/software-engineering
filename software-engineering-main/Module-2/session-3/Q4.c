961163d548b47b4513282f723b9acce74bb485fe
unknown <mauryavidhyasagar6549@gmail.com>
2026-08-04 23:28:25 -0700

    all

--- software-engineering-main/Module-2/session-3/Q4.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-main(){
-	int FirstPlayer;// corrected (1stplayer)
-	int player_age;
-	int score;// corrected($score)
-	int total_marks;// corrected(total-marks)
-	char userName;
-}
