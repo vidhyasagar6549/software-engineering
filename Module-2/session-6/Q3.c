85b40bb75f9702ba3b4cd056f2ccf15857cfaacb
vidhyasagar6549 <mauryavidhyasagar6549@gmail.com>
2026-08-08 14:01:22 +0530

    Q3.c

--- Module-2/session-6/Q3.c
+++ Module-2/session-6/Q3.c
@@ -1,10 +1,31 @@
-#include<stdio.h>
-main(){
-	char song[30][20]={"believer","shape of you","prefect"};
-	int 1,guess;
-	rand(song);
-	do{
-		if(song)
-		
-	}while();
+#include <stdio.h>
+
+main()
+{
+    int choice;
+    int guess;
+
+    printf("Guess the Song\n");
+    printf("1. Kesariya\n");
+    printf("2. Apna Bana Le\n");
+    printf("3. Tum Hi Ho\n");
+
+    choice = 2;
+
+    do
+    {,
+        printf("\nEnter your guess (1-3): ");
+        scanf("%d", &guess);
+
+        if (guess == choice)
+        {
+            printf("Correct! You guessed the song.\n");
+        }
+        else
+        {
+            printf("Wrong! Try again.\n");
+        }
+
+    } while (guess != choice);
+
 }
