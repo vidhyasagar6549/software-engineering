247c3166335cca9d82f43be4f394fdc5924a74c3
vidhyasagar6549 <mauryavidhyasagar6549@gmail.com>
2026-08-09 15:24:01 +0530

    Q4.c

---
+++ Module-2/session-9/Q4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+ main() {
+    int cricketScores[3][2] = {
+        {180, 175},
+        {210, 190},
+        {165, 200}
+    };
+
+    for (int i = 0; i < 3; i++) {
+        if (cricketScores[i][0] > cricketScores[i][1])
+            printf("Match %d highest score = %d\n", i + 1, cricketScores[i][0]);
+        else
+            printf("Match %d highest score = %d\n", i + 1, cricketScores[i][1]);
+    }
+
+    
+}
