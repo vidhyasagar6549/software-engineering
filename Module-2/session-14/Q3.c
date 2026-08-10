54531cc8606a5a091d2d80126ae202b3a9f94fce
vidhyasagar6549 <mauryavidhyasagar6549@gmail.com>
2026-08-10 12:57:49 +0530

    Q3.c

--- Module-2/session-14/Q3.c
+++ Module-2/session-14/Q3.c
@@ -1,14 +1,19 @@
-#include<stdio.h>
-void formatFollowersCount(int count){
-	if(count>=100000){
-		printf("%dm",count/100000);
-	}else if(count > = 1000)
-	{
-		printf("%dk",count/1000);
-	}else{
-		printf("\n ")
-	}
+#include <stdio.h>
+
+void formatFollowersCount(int count) {
+
+    if (count >= 100000) {
+        printf("%dm", count / 100000);
+    }
+    else if (count >= 1000) {
+        printf("%dk", count / 1000);
+    }
+    else {
+        printf("%d", count);
+    }
 }
-main(){
-	
+
+main() {
+
+    formatFollowersCount(5000000);
 }
