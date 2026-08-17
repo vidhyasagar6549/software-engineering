f4e53b93c70eddf6571a9296bc9eea0ef4ed17ed
aaad <aaadsuryavanshi@gmail.com>
2026-08-16 22:58:44 -0700

    module-3

---
+++ Module-3/session-1/Q1.cpp
@@ -0,0 +1,13 @@
+ #include<iostream>
+ using namespace std;
+ main(){
+    char string[5][50];
+ 	int i;
+ 	for(i=0;i<5;i++){
+ 	cout<<" Enter your string:-"; 
+ 	cin>>string[i];
+ 	}
+ 	for(i=0;i<5;i++){
+ 		 cout<<"\n Enter your string:-"<<string[i];
+	 }
+}
