1de8649563e60f558e0148c2ccdc7ea6c5d15a71
vidhyasagar6549 <mauryavidhyasagar6549@gmail.com>
2026-09-07 12:09:04 +0530

    Q1.cpp

--- Module-3/session-7/Q1.cpp
+++ Module-3/session-7/Q1.cpp
@@ -1,7 +1,18 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
 using namespace std;
-class{
-};
-main(){
-	
+
+ main() {
+
+    ofstream file("my_fav_songs.txt");
+
+    file << "Perfect" << endl;
+    file << "Shape of You" << endl;
+    file << "Believer" << endl;
+    file << "Blinding Lights" << endl;
+    file << "Faded" << endl;
+
+    file.close();
+
+    cout << "Songs written to file successfully.";
 }
